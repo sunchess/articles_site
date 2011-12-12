4012180cc9780f9b80c4ea0907101b32bb928756
sunchess <sunchess@inbox.ru>
2011-12-12 21:08:30 +0400

    add store breadcrambs

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,5 @@
 class StoresController < ApplicationController
+  before_filter :add_breadcrumb
   def index
     @stores = Store.shown
   end
@@ -6,6 +7,11 @@ class StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     add_breadcrumb @store.name, store_path(@store)
+  end
+
+  private
+  def add_breadcrumb
+    add_breadcrumb "Магазин", stores_path
   end
 
 end
