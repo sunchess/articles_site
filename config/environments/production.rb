68af535ea41af0f1dd6353100f288ec6abaaf05b
sunchess <sunchess@inbox.ru>
2011-12-18 20:21:45 +0400

    added host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Jsovet::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'jsovet.ru' }
+  config.default_url_options[:host] = "jsovet.ru"
+
 end
