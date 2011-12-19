f08c4d1bb934314e4cc94de862c82398e57e2edd
sunchess <sunchess@inbox.ru>
2011-12-19 22:24:25 +0400

    some changes

--- app/helpers/shoping_carts_helper.rb
+++ app/helpers/shoping_carts_helper.rb
@@ -2,7 +2,7 @@ module ShopingCartsHelper
   def value_h(v)
     case v.to_i
     when 1000
-      "1 килограмм(1000 грамм)"
+      "1 килограмм"
     when 1400
       "1 литр(1400 грамм)"
     when 4200
