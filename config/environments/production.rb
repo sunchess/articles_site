e5a52f3e7efbc44a38fc8edea51d3f328604abc5
sunchess <sunchess@inbox.ru>
2011-12-20 00:17:11 +0400

    delete host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,6 @@ Jsovet::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { :host => 'jsovet.ru' }
-  config.default_url_options[:host] = "jsovet.ru"
+  #config.default_url_options[:host] = "jsovet.ru"
 
 end
