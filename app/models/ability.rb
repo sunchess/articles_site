e7c988f04b0d42c0dc5f11512b7ec25306790ff3
sunchess <sunchess@inbox.ru>
2012-01-21 20:52:13 +0400

    fix cancan ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
     else
       can :read, [Category, Article, Comment]
       if user.confirmed_at #is real user
-        can :create, Camment
+        can :create, Comment
         can :create, Article
         can :update, Article, { :user_id => user.id, :status => :draft }
       end
