cbc4b0a9a8f305e61fa9ff67fcea4fe17f87edab
sunchess <sunchess@inbox.ru>
2012-01-21 23:16:14 +0400

    added home page order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.published.paginate(:per_page => 20, :page => params[:page])
+    @articles = Article.published.order("updated_at DESC").paginate(:per_page => 20, :page => params[:page])
   end
 
 end
