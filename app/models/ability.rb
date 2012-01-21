4f64f59aeb25b5372988391356320ce6ca3b3be7
sunchess <sunchess@inbox.ru>
2012-01-21 22:36:09 +0400

    fix ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,6 +12,7 @@ class Ability
         can :create, Comment
         can :create, Article
         can :update, Article, { :user_id => user.id, :aasm_status => :draft }
+        can :my, Article
       end
     end
 
