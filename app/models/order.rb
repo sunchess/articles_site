9d7451e42586f0080a914eeeff60a26919264edf
sunchess <sunchess@inbox.ru>
2012-03-15 13:34:35 +0400

    added email to order; little changes in order notice

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,20 +1,20 @@
-class Order 
+class Order
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
-  
-  attr_accessor :name, :telephone, :content, :address, :dtime 
-  
+
+  attr_accessor :name, :telephone, :content, :address, :dtime, :email
+
   validates_presence_of :name, :telephone, :address, :dtime
-  #validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_length_of :content, :maximum => 1000
-  
+
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
     end
   end
-  
+
   def persisted?
     false
   end
