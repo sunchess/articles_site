cdb38ee2a188b40a3cf60e2651f2f89ed4e72015
Alex Dmitriev <sunchess@inbox.ru>
2012-05-07 12:25:30 +0400

    added title and desctiptions tags on layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,6 +52,16 @@ module ApplicationHelper
       ""
     end
   end
+
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(page_description)
+    content_for(:description) {  page_description }
+  end
+
 end
 
 
