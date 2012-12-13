40cd7aaf1267a752ffc6f718efceb3b41d3c7dfd
SashaChilly <theiriskin@gmail.com>
2012-12-13 23:55:48 +0400

    Add sorting to stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,8 @@
 class StoresController < ApplicationController
   before_filter :add_crumb, :only => :show
+  after_filter :authorize, :only => [:sort]
+  load_and_authorize_resource :store, :except => [:sort]
+
   def index
     @stores = Store.shown
   end
@@ -7,6 +10,14 @@ class StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     add_breadcrumb @store.name, store_path(@store)
+  end
+
+  def sort
+    params[:store].each_with_index do |id, index|
+      Store.update_all({:position => index+1}, {:id => id})
+    end
+  render :nothing => true
+  
   end
 
   private
