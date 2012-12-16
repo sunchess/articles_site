09e421ae284fab02ef139cd39ae8bb2f08a02583
SashaChilly <theiriskin@gmail.com>
2012-12-16 22:17:00 +0400

    refactor

---
+++ public/javascripts/sort_a.js
@@ -0,0 +1,15 @@
+// Sorting the list
+
+jQuery(function() {
+  return $('#articles').sortable({
+    axis: 'y',
+    handle: '.handle',
+    dropOnEmpty: false,
+    cursor: 'crosshair',
+    opacity: 0.4,
+    scroll: true,
+    update: function() {
+      return $.post($(this).data('update-url'), $(this).sortable('serialize'));
+    }
+  });
+});
---
+++ public/javascripts/sort_s.js
@@ -0,0 +1,16 @@
+
+// Sorting the list
+
+jQuery(function() {
+  return $('#stores').sortable({
+    axis: 'y',
+    handle: '.handle',
+    dropOnEmpty: false,
+    cursor: 'crosshair',
+    opacity: 0.4,
+    scroll: true,
+    update: function() {
+      return $.post($(this).data('update-url'), $(this).sortable('serialize'));
+    }
+  });
+});
