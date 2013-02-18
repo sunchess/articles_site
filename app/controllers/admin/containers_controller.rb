11ea76a79fd5dc13d9027612178fa75e0f0bf403
SashaChilly <theiriskin@gmail.com>
2013-02-18 23:40:50 +0400

    change text editor for containers and stores

--- app/controllers/admin/containers_controller.rb
+++ app/controllers/admin/containers_controller.rb
@@ -1,6 +1,15 @@
 class Admin::ContainersController < ApplicationController
   load_and_authorize_resource :container
-
+  uses_tiny_mce :options => {
+                            :theme => 'advanced',
+                            :theme_advanced_resizing => true,
+                            :theme_advanced_resize_horizontal => false,
+                            :theme_advanced_toolbar_location => "top",
+                            :theme_advanced_toolbar_align => "left",
+                            :theme_advanced_statusbar_location => "bottom",
+                            :theme_advanced_buttons1 => %w{ bold italic underline fontselect fontsizeselect formatselect newdocument},
+                            :theme_advanced_buttons2 => %w{ justifyleft justifycenter justifyright bullist numlist undo redo }
+                            }
   def index
     @containers = Container.order(:position)
     authorize! :update, @containers
--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,5 +1,15 @@
 class Admin::StoresController < ApplicationController
   load_and_authorize_resource :store
+  uses_tiny_mce :options => {
+                            :theme => 'advanced',
+                            :theme_advanced_resizing => true,
+                            :theme_advanced_resize_horizontal => false,
+                            :theme_advanced_toolbar_location => "top",
+                            :theme_advanced_toolbar_align => "left",
+                            :theme_advanced_statusbar_location => "bottom",
+                            :theme_advanced_buttons1 => %w{ bold italic underline fontselect fontsizeselect formatselect newdocument},
+                            :theme_advanced_buttons2 => %w{ justifyleft justifycenter justifyright bullist numlist undo redo }
+                            }
 
   def index
     @stores = Store.order(:position)
